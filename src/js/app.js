afd84272c96faf0d2a7b4ac9b1b2c63b3d45a702
noinvis <lazizbek.sn@gmail.com>
2025-05-29 00:20:11 +0500

    Second

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,37 @@
 const sidebarEl = document.querySelector(".sidebar")
 const voiceEl = document.querySelector(".voice__click")
 const addEl = document.querySelector(".add__click")
-
+const notificationEl = document.querySelector(".notification__click")
+const dotsEl = document.querySelector(".dots__click")
+const shareEl = document.querySelector(".share__click")
 
 function showSidebar(){
     sidebarEl.classList.toggle("active")
 }
-
 function clickVoice(){
     voiceEl.classList.add("active")
 }
-
 function closeVoice(){
     voiceEl.classList.remove("active")
 }
-
 function showAdd(){
     addEl.classList.toggle("active")
 }
-
+function showNotification(){
+    notificationEl.classList.toggle("active")
+}
+function showProfile(){
+    profileEl.classList.toggle("active")
+}
+function openDots(){
+    dotsEl.classList.toggle("active")
+}
+function shareClick(){
+    shareEl.classList.add("active")
+}
+function shareClick(){
+    shareEl.classList.remove("active")
+}
 
 let offset = 53;
 
